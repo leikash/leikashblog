f377a8ea008d8d160f6ee83e9770f58aecebe519
leikash <yukio.yamada.yy@gmail.com>
2022-03-06 11:31:19 +0800

    add picture in the main list and add sorting in graphql

--- leikash-site/src/pages/index.js
+++ leikash-site/src/pages/index.js
@@ -11,7 +11,17 @@ const BlogPage = ({ data }) => {
           <ul>
             {data.allMicrocmsBlog.edges.map(({ node }) => (
               <dir key={node.blogId}>
+                <span>{node.revisedAt}</span>
+                <img src={node.mainVisual.url}
+                  alt="Main visual"
+                  width="10%"
+                />
                 <Link className="blogTitle" to={`/blog/${node.blogId}`}>{node.title}</Link>
+                <div
+                  dangerouslySetInnerHTML={{
+                  __html: `${node.summary}`,
+                  }}
+                />
               </dir>
             ))}
           </ul>
@@ -22,13 +32,23 @@ const BlogPage = ({ data }) => {
 
 export default BlogPage
 
+// graphql ソート
+// https://qiita.com/atomyah/items/759823f6c1e86ce44b23
+
 export const query = graphql`
   query {
-    allMicrocmsBlog {
+    allMicrocmsBlog(sort: { fields: [revisedAt], order: DESC }) {
       edges {
         node {
           blogId
+          revisedAt
           title
+          summary
+          mainVisual {
+            url
+            height
+            width
+          }
         }
       }
     }
